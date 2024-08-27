2e8094cfc5747736db19abf8e8dcd7ec9b5fd122
Muhammad Dava Fahreza <mdavafahreza05@gmail.com>
2024-08-27 13:19:11 +0700

    feat: basic typesript prymitive and function

---
+++ index.ts
@@ -0,0 +1,29 @@
+// ! primitive types
+// const number: number = 1
+// const string: string = "string"
+// const bolean: boolean = false
+
+// console.log(number, string, bolean)
+
+// ! types array
+// const arrayNumbers: number[] = [1, 2, 3, 4, 5, 6, 7, 8]
+// const stringNumbers: string[] = ["1", "2", "3", "4", "5", "6", "7", "8"]
+// const boleanNumbers: boolean[] = [false, true, false, true]
+// console.log(arrayNumbers, stringNumbers, boleanNumbers)
+
+// ! types any
+// let any: any = "any"
+// any = 2
+// console.log(any)
+
+
+// ! funcitons
+// function greet(name: string, message: string, callback: Function) {
+//     callback(name, message)
+// }
+
+// function greeting(name: string, message: string) {
+//     console.log(`hallo ${name},  ${message}`)
+// }
+
+// greet("dava", "kamu goblok sekali everybody", greeting)
