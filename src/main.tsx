68227f36a01d1e48481f2f85261733973bdc5378
Muhammad Dava Fahreza <mdavafahreza05@gmail.com>
2024-09-21 12:56:52 +0700

    fix: basic layout

--- src/main.tsx
+++ src/main.tsx
@@ -3,16 +3,13 @@ import { createRoot } from "react-dom/client";
 import { ChakraProvider } from "@chakra-ui/react";
 import AppRouter from "./config/React-Router-Domm.tsx";
 import { ThemeConfig } from "./config/chakra-theme.ts";
-import { Provider } from "react-redux";
 import "./assets/css/fonts.css";
 import "./assets/css/scrollbar.css";
 
 createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
-    <Provider store={{}}>
-      <ChakraProvider theme={ThemeConfig}>
-        <AppRouter></AppRouter>
-      </ChakraProvider>
-    </Provider>
+    <ChakraProvider theme={ThemeConfig}>
+      <AppRouter></AppRouter>
+    </ChakraProvider>
   </React.StrictMode>,
 );
