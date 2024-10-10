0a2145b09c1bc6682d47d42c545061e84d01d246
dava <mdavafahrez05@gmail.com>
2024-10-10 20:05:14 +0700

    fix: env fix

--- src/lib/api-v1.ts
+++ src/lib/api-v1.ts
@@ -1,12 +1,12 @@
 import axios from "axios"
 import Cookie from "js-cookie"
-// const baseURL = import.meta.env.VITE_BACKEND_API
+const baseURL = import.meta.env.VITE_BACKEND_API
 const token = "Bearer " + Cookie.get("token")
 
 
 export const apiV1 = axios.create(
     {
-        baseURL : "https://be-circle-one.vercel.app/api/v1",
+        baseURL,
         headers: {
             Authorization: token
         }
