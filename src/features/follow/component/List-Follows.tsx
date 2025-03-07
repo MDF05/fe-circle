5114ad8415197c9d2c7716eaff0d57a33dbbfa11
Muhammad Dava Fahreza <mdavafahreza05@gmail.com>
2025-03-07 16:02:46 +0700

    fix: list follow features

--- src/features/follow/component/List-Follows.tsx
+++ src/features/follow/component/List-Follows.tsx
@@ -1,16 +1,16 @@
 import { Flex } from "@chakra-ui/react";
 import FollowComponent from "./Follow";
 import { useListFollows } from "../hooks/use-list-follow";
-// import { LoadingPage } from "../../../component/Loading-Page";
+import { LoadingPage } from "../../../component/Loading-Page";
 
 export default function FollowsFeatures({ location, limit }: { location: string; limit?: string }) {
   const { profiles } = useListFollows(limit);
 
-  // if(profiles.length == 0) return (
-  //   <Flex width={"100%"} h={"100%"}  justifyItems={"center"} alignItems={"center"} justifyContent={"center"}>
-  //     <LoadingPage></LoadingPage>
-  //   </Flex>
-  // )
+  if(profiles.length == 0) return (
+    <Flex width={"100%"} h={"100%"}  justifyItems={"center"} alignItems={"center"} justifyContent={"center"}>
+      <LoadingPage></LoadingPage>
+    </Flex>
+  )
 
   return (
     <Flex flexDirection="column" gap="10px" background={location != "follows" ? "brand.backgroundYoung" : "transparent"} color="white" rounded={"10px"}>
